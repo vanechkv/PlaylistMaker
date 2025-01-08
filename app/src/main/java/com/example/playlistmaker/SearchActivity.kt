41745159ea3aca30f7a33d7572d32b02cce9cf27
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-01-08 22:04:29 +0500

    Second Homework

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,92 @@
 package com.example.playlistmaker
 
+import android.app.Application
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var inputEditText: EditText
+    private var searchText: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+
+        val clearButton = findViewById<ImageView>(R.id.search_close_btn)
+        inputEditText = findViewById(R.id.search_edit_text)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+
+            inputEditText.clearFocus()
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                searchText = s?.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        }
+
+        inputEditText.addTextChangedListener(textWatcher)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT)
+        inputEditText.setText(searchText)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +22,39 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+
+        val shareFrameLayout = findViewById<FrameLayout>(R.id.share_frame_layout)
+
+        shareFrameLayout.setOnClickListener {
+            val shareText = getString(R.string.url_ya_praktikum_android_developer)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            shareIntent.setType("text/plain")
+            startActivity(shareIntent)
+        }
+
+        val supportFrameLayout = findViewById<FrameLayout>(R.id.support_frame_layout)
+
+        supportFrameLayout.setOnClickListener {
+            val supportText = getString(R.string.support_text)
+            val supportSubject = getString(R.string.support_subject)
+            val supportEmail = getString(R.string.support_email)
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(supportEmail))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, supportSubject)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, supportText)
+            startActivity(supportIntent)
+        }
+
+        val userAgreementFrameLayout = findViewById<FrameLayout>(R.id.user_agreement_frame_layout)
+
+        userAgreementFrameLayout.setOnClickListener {
+            val urlUserAgreement = getString(R.string.url_user_agreement)
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW)
+            userAgreementIntent.data = Uri.parse(urlUserAgreement)
+            startActivity(userAgreementIntent)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
