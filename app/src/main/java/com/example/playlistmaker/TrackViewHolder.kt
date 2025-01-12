cf2a3b50e6593e23eaa51008b0c63d133bef71a8
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-01-13 01:17:45 +0500

    Third Homework

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,32 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+) {
+
+    private val trackName: TextView = itemView.findViewById(R.id.track_name)
+    private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+    private val artworkUrl100: ImageView = itemView.findViewById(R.id.artwork)
+
+    fun bind(model: Track) {
+
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .transform(CenterCrop(), RoundedCorners(2))
+            .into(artworkUrl100)
+    }
+}
